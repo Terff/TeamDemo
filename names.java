54e393426dce93f622c4626728288a6ef6ffffbc
peterlowrance <peter.lowrance@gmail.com>
2019-02-21 12:39:09 -0500

    added name Peter

--- names.java
+++ names.java
@@ -4,5 +4,10 @@ public class names
   public static void main(String[] args)
   {
     System.out.println("Our names are...");
+    PeterPrint();
+  }
+  
+  private void PrintPeter(){
+        System.out.println("My name is Peter");
   }
 }
