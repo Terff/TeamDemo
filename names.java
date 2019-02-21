1973abf66c3148fc0b1931a2c9f1408bddfbf146
David Corbin <daconex@gmail.com>
2019-02-21 12:44:44 -0500

    Added David method

--- names.java
+++ names.java
@@ -4,5 +4,10 @@ public class names
   public static void main(String[] args)
   {
     System.out.println("Our names are...");
+    printDavid();
+  }
+
+  public static void printDavid() {
+    System.out.println("David");
   }
 }
