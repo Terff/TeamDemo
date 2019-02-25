2db471375ef3f8abb6532a1b1ca8f0fa9660dab2
Ethan Weaver <terffinatorz@gmail.com>
2019-02-25 16:20:45 -0500

    reset

--- names.java
+++ names.java
@@ -4,25 +4,9 @@ public class names
   public static void main(String[] args)
   {
     System.out.println("Our names are...");
-    printNameEthan();
-    printDavid();
-    PeterPrint();
-  	printM();
-  }
-
-  public static void printM() {
-    System.out.println("mitch");
-  }
-  public static void printNameEthan()
-  {
-    System.out.println("Ethan Weaver")
-  }
-
-  public static void printDavid() {
-    System.out.println("David");
-  }
-
-  private void PrintPeter(){
     System.out.println("My name is Peter");
+    System.out.println("mitch");
+    System.out.println("Ethan Weaver")
+    System.out.println("David");
   }
 }
