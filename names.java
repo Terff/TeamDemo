501afd61549ec88a96ab7768728a172abeb8b30f
Mitchell Harvey <mph121@users.noreply.github.com>
2019-02-25 16:25:21 -0500

    merge mitch with david

--- names.java
+++ names.java
@@ -5,11 +5,16 @@ public class names
   {
     System.out.println("Our names are...");
     System.out.println("My name is Peter");
-    System.out.println("mitch");
-    System.out.println("Ethan Weaver")
+    System.out.println("Ethan Weaver");
+   pm();
+  printDavid();
   }
 
   public static void printDavid() {
 	  System.out.println("David");
   }
+	public static void pm(){
+
+    System.out.println("mitch");
+}
 }
