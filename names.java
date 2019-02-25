5405973623ab34a9c818614b2bf4ed5251bbc33a
David Corbin <daconex@gmail.com>
2019-02-25 16:28:06 -0500

    Merged david

--- names.java
+++ names.java
@@ -4,16 +4,16 @@ public class names
   public static void main(String[] args)
   {
     System.out.println("Our names are...");
-    System.out.println("Ethan Weaver");
-   pm();
-  printDavid();
+    pm();
+    printDavid();
     PeterPrint();
   }
 
   public static void printDavid() {
 	  System.out.println("David");
   }
-	public static void pm(){
+
+  public static void pm(){
 
     System.out.println("mitch");
 }
