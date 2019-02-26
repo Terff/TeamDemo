cd12e76b4033154ae1517f60ce143c22ec11a493
Ethan Weaver <terffinatorz@gmail.com>
2019-02-26 11:44:12 -0500

    Add files via upload

---
+++ names.java
@@ -0,0 +1,12 @@
+
+public class names
+{
+  public static void main(String[] args)
+  {
+    System.out.println("Our names are...");
+    System.out.println("David Corbin");
+    System.out.println("mitch");
+    System.out.println("Peter Lowrance");
+    System.out.println("Ethan Weaver")
+  }
+}
