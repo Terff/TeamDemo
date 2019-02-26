1345978753cdb5073ca46a26a079d966c5314cad
David Corbin <daconex@gmail.com>
2019-02-26 11:46:41 -0500

    Added printDavid

--- names.java
+++ names.java
@@ -4,9 +4,13 @@ public class names
   public static void main(String[] args)
   {
     System.out.println("Our names are...");
-    System.out.println("David Corbin");
+    printDavid();
     System.out.println("mitch");
     System.out.println("Peter Lowrance");
     System.out.println("Ethan Weaver")
+  }
+
+  public static void printDavid() {
+	System.out.println("David J. Corbin");
   }
 }
