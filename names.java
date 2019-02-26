b3f4e824c4947f09830e7a1fc366f8412c26286d
Ethan Weaver <terffinatorz@gmail.com>
2019-02-26 11:46:29 -0500

    Ethan

--- names.java
+++ names.java
@@ -4,26 +4,13 @@ public class names
   public static void main(String[] args)
   {
     System.out.println("Our names are...");
-    pm();
-    printDavid();
-    PeterPrint();
-    printEthan();
-  }
-
-  public static void printDavid() {
-	  System.out.println("David Corbin");
-  }
-
-  public static void pm(){
-
+    System.out.println("David Corbin");
     System.out.println("mitch");
-}
-
-  private static void PeterPrint(){
-        System.out.println("Peter Lowrance");
+    System.out.println("Peter Lowrance");
+    printEthan();
   }
   public void printEthan()
   {
-    System.out.println("Ethan P Weaver")
+    System.out.println("Ethan M. Weaver");
   }
 }
